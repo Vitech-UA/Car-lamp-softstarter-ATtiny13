eaae2ecf965cb1175a2a6d75efbaf334a1afa0e6
Vitech-UA <viktorstarovit@gmail.com>
2020-11-24 22:44:38 +0200

    First commit

---
+++ ATtiny13_PWM_Softstarter/ATtiny13_PWM_Softstarter/main.c
@@ -0,0 +1,18 @@
+/*
+ * ATtiny13_PWM_Softstarter.c
+ *
+ * Created: 24.11.2020 22:42:41
+ * Author : Embed Viktor
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Car-lamp-softstarter-ATtiny13
